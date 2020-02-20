1070e7c324e2929439cf482d0aeb684e76f52feb
arjaison <60999473+arjaison@users.noreply.github.com>
2020-02-21 01:04:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('modelfinchange.pkl', 'rb'))
+model = pickle.load(open('modelfinal.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,20 +16,15 @@ def predict():
     '''
     int_features = [str(x) for x in request.form.values()]
     #print(type(int_features))
-    #print("Helooo     ",int_features)
+   
     
     final_features = np.array(int_features)
-    #print(type(final_features))
-    #final_features=final_features.reshape(1,-1)  
+   
     final_features=np.delete(final_features,(0,2,7,8,10,12,22))
     final_features=[int(i) for i in final_features] 
-    
-    
-    #print("Byeeeeee     ",final_features)
-    #print(type(final_features))
-    #final_features=final_features.delete['What is your name?','Which school are you studying in?','What is your favourite food?','How do you workout everyday?','How do you start your day?','How do you usually feel after a nap?', 'Mention a health problem which troubles you often']
+
     prediction = model.predict([final_features])
-    #print(type(prediction))
+    
     print(prediction)
     output = prediction
 
@@ -37,4 +32,5 @@ def predict():
 
 
 if __name__ == "__main__":
+   
     app.run(debug=True)
